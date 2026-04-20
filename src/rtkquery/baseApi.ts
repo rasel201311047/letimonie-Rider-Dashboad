94aaf044b2c74a5a933ea6e8000f715cda105177
rasel201311042 <mdraselbabu0367@gmail.com>
2026-04-20 17:00:55 +0600

    api cunnect

--- src/rtkquery/baseApi.ts
+++ src/rtkquery/baseApi.ts
@@ -3,8 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://18.140.140.211:5550/api/v1",
+    baseUrl: "https://letty-ride-share-backend-1.onrender.com/api/v1",
+    // "http://18.140.140.211:5550/api/v1",
     // "http://10.10.20.24:5550/api/v1",
+    // https://letty-ride-share-backend-1.onrender.com/
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       const token = localStorage.getItem("accessToken");

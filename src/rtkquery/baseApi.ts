7390be58e79a6f54be7261e8ff1d4b16477bf374
rasel201311042 <mdraselbabu0367@gmail.com>
2026-04-20 12:57:04 +0600

    done

--- src/rtkquery/baseApi.ts
+++ src/rtkquery/baseApi.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://10.10.20.24:5550/api/v1",
+    baseUrl: "http://18.140.140.211:5550/api/v1",
+    // "http://10.10.20.24:5550/api/v1",
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       const token = localStorage.getItem("accessToken");

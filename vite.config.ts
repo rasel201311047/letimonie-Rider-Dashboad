1d4ac2a635cdb796da5965fc2b7e566d3ce9d1cf
rasel201311042 <mdraselbabu0367@gmail.com>
2026-05-05 10:20:22 +0600

    http host in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://18.140.140.211:5550",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+      },
+    },
+  },
 });

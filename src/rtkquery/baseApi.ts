38a8c6b53e9fa2d6a99e89e14c8152dd74c94a7d
rasel201311042 <mdraselbabu0367@gmail.com>
2026-05-05 11:31:45 +0600

    vercel host

--- src/rtkquery/baseApi.ts
+++ src/rtkquery/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    baseUrl: "https://letty-ride-share-backend-1.onrender.com/api/v1",
 
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");

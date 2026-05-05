084b823e9f1cf8ab106c0e0ff5346b6d4bc6cc50
rasel201311042 <mdraselbabu0367@gmail.com>
2026-05-05 10:43:18 +0600

    fix vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // ✅ Vercel looks for this
+  },
   server: {
     proxy: {
       "/api": {

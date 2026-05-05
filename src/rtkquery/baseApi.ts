c053041fcf1c8619ff12472fb0a8c45cc6e00848
rasel201311042 <mdraselbabu0367@gmail.com>
2026-05-05 11:28:05 +0600

    the proxy cannot do that reason the render api use

--- src/rtkquery/baseApi.ts
+++ src/rtkquery/baseApi.ts
@@ -4,10 +4,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_BASE_URL,
-    // "https://letty-ride-share-backend-1.onrender.com/api/v1",
-    // "http://18.140.140.211:5550/api/v1",
-    // "http://10.10.20.24:5550/api/v1",
-    // https://letty-ride-share-backend-1.onrender.com/
+
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       const token = localStorage.getItem("accessToken");
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    proxy: {
-      "/api/proxy": {
-        target: "http://18.140.140.211:5550/api/v1",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/proxy/, ""),
-      },
-    },
-  },
 });
